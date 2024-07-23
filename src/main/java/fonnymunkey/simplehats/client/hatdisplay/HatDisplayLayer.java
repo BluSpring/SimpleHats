1f16517e5584a283efcea003b9e9c5b5b0f7a425
fonnymunkey <fonnymunkey@gmail.com>
2024-07-22 21:20:24 -0400

    tired of animation mod incompatibility errors

--- src/main/java/fonnymunkey/simplehats/client/hatdisplay/HatDisplayLayer.java
+++ src/main/java/fonnymunkey/simplehats/client/hatdisplay/HatDisplayLayer.java
@@ -1,15 +1,17 @@
 package fonnymunkey.simplehats.client.hatdisplay;
 
-import dev.emi.trinkets.api.client.TrinketRendererRegistry;
 import fonnymunkey.simplehats.common.entity.HatDisplay;
 import fonnymunkey.simplehats.common.item.HatItem;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Vec3f;
 
 public class HatDisplayLayer<T extends LivingEntity, M extends EntityModel<T>> extends FeatureRenderer<T, M> {
 
@@ -23,26 +25,21 @@ public class HatDisplayLayer<T extends LivingEntity, M extends EntityModel<T>> e
     @Override
     public void render(MatrixStack matrixStack, VertexConsumerProvider renderTypeBuffer, int light, T livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         if(livingEntity instanceof HatDisplay display) {
-            matrixStack.push();
             ItemStack stack = display.getEquippedStack(null);
-            if(!stack.isEmpty() && stack.getItem() instanceof HatItem) {
-                TrinketRendererRegistry.getRenderer(stack.getItem()).ifPresent(
-                        renderer -> renderer
-                                .render(stack,
-                                        null,
-                                        this.getContextModel(),
-                                        matrixStack,
-                                        renderTypeBuffer,
-                                        light,
-                                        livingEntity,
-                                        limbSwing,
-                                        limbSwingAmount,
-                                        partialTicks,
-                                        ageInTicks,
-                                        netHeadYaw,
-                                        headPitch));
+            if(!stack.isEmpty() && stack.getItem() instanceof HatItem && !livingEntity.isInvisible()) {
+                matrixStack.push();
+                
+                matrixStack.scale(1.01F, 1.01F, 1.01F);
+                matrixStack.translate(0D, 0.97D, 0.0D);
+                
+                matrixStack.scale(0.66F, 0.66F, 0.66F);
+                matrixStack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(180.0F));
+                matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180.0F));
+                
+                MinecraftClient.getInstance().getHeldItemRenderer().renderItem(livingEntity, stack, ModelTransformation.Mode.HEAD, false, matrixStack, renderTypeBuffer, light);
+                
+                matrixStack.pop();
             }
-            matrixStack.pop();
         }
     }
 }
